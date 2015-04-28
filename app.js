14d252a3c228e9e48da670071ddb512557b4054a
Niclas Jareborg <niclas@jareborg.se>
2015-04-28 16:46:49 +0200

    Added file

---
+++ app.js
@@ -0,0 +1,57 @@
+(function(){
+
+  var app = angular.module('enaMetadata', []);
+
+  app.controller('StudyController', ['$scope',function($scope) {
+    // this.items = studyItems;
+    $scope.shortName = {
+      label: 'Short name',
+      description: 'A short descriptive name for the project',
+      value: ''
+    };
+
+    $scope.title = {
+      label: 'Title',
+      description: 'A short description of the project akin to an article title',
+      value: ''
+    };
+
+    $scope.studyType = {
+      label: 'Study type',
+      description: '',
+      options: [
+        "Whole Genome Sequencing",
+        "Metagenomics",
+        "Transcriptome Analysis",
+        "Resequencing",
+        "Epigenetics",
+        "Synthetic Genomics",
+        "Forensic or Paleo-genomics",
+        "Gene Regulation Study",
+        "Cancer Genomics",
+        "Population Genomics",
+        "RNASeq",
+        "Exome Sequencing",
+        "Pooled Clone Sequencing",
+        "Other"
+      ],
+      value: 'Whole Genome Sequencing'
+    };
+
+
+  }]);
+
+  // var studyItems = [
+  //   {
+  //     name: 'Short name',
+  //     type: 'text',
+  //     default: 'A short descriptive name for the project'
+  //   },
+  //   {
+  //     name: 'Title',
+  //     type: 'text',
+  //     default: 'A short description of the project akin to an article title'
+  //   }
+  // ];
+
+})();
